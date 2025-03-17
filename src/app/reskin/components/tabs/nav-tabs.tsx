f07d60e1d1e74cff5fac036e55b84f7091eff898
hz <13457698+hozzjss@users.noreply.github.com>
2025-03-17 18:19:52 +0200

    fix reskin responsiveness

--- src/app/reskin/components/tabs/nav-tabs.tsx
+++ src/app/reskin/components/tabs/nav-tabs.tsx
@@ -18,11 +18,11 @@ const tabs = [
 export const NavTabs = () => {
   const pathname = usePathname();
   return (
-    <div className="fixed bottom-0 sm:sticky dark:bg-reskin-dark-gray w-full">
-      <div className="flex h-20 flex-row justify-between items-center w-full sm:mx-auto max-w-5xl relative z-10">
+    <div className="fixed bottom-0 md:sticky dark:bg-reskin-dark-gray w-full">
+      <div className="flex h-20 flex-row justify-between items-center w-full md:mx-auto max-w-5xl relative z-10">
         {tabs.map((tab, index) => (
           <Link
-            className={`font-matter-mono h-full uppercase flex-1 flex justify-center items-center ${pathname === tab.link ? "border-t-orange sm:border-b-orange dark:border-t-dark-reskin-orange font-bold dark:sm:border-b-dark-reskin-orange" : "border-t-transparent sm:border-b-[#D9D9D9]"} border-t-4 sm:border-b-4 sm:border-t-0 sm:bottom-auto`}
+            className={`font-matter-mono h-full uppercase flex-1 flex justify-center items-center ${pathname === tab.link ? "border-t-orange md:border-b-orange dark:border-t-dark-reskin-orange font-bold dark:md:border-b-dark-reskin-orange" : "border-t-transparent md:border-b-[#D9D9D9]"} border-t-4 md:border-b-4 md:border-t-0 md:bottom-auto`}
             key={index}
             href={tab.link}
           >
@@ -30,7 +30,7 @@ export const NavTabs = () => {
           </Link>
         ))}
       </div>
-      <hr className="hidden sm:block w-full border-t-light-reskin-border-gray dark:border-t-dark-reskin-border-gray relative bottom-1" />
+      <hr className="hidden md:block w-full border-t-light-reskin-border-gray dark:border-t-dark-reskin-border-gray relative bottom-1" />
     </div>
   );
 };
